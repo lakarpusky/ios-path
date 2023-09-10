dd1d00e3f035159f60f87505c55c430b45f7102c
Gabø.montero <gabo.montero89@gmail.com>
2023-09-10 01:52:36 -0400

    Day 19: Challenge day

---
+++ 100 Days of SwiftUI/Day 19/UnitConversions/UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by ☕️ Gabo.montero on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
