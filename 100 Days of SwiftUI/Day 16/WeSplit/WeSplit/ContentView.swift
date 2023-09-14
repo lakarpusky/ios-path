b264f96a0521a11d49a6b3d8e81eb89b4241f942
Gabø.montero <gabo.montero89@gmail.com>
2023-09-14 18:30:56 -0400

    Day 24: Challenge (Project 1)

--- 100 Days of SwiftUI/Day 16/WeSplit/WeSplit/ContentView.swift
+++ 100 Days of SwiftUI/Day 16/WeSplit/WeSplit/ContentView.swift
@@ -70,6 +70,7 @@ struct ContentView: View {
                             code: Locale.current.currency?.identifier ?? "USD"
                         )
                     )
+                    .foregroundColor(tipPercentage == 0 ? .red : .primary)
                 } header: {
                     Text("Total amount for the check")
                 }
