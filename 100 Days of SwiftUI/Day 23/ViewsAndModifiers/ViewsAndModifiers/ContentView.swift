1fd08716ea2c3a0cf1505b090f768086acaf524c
Gabø.montero <gabo.montero89@gmail.com>
2023-09-14 18:14:38 -0400

    Day 23: Project 3, part one
    - Conditional modifiers
    - Environment modifiers
    - Views as properties
    - Custom modifiers
    - Custom containers*

---
+++ 100 Days of SwiftUI/Day 23/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,161 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by ☕️ Gabo.montero on 9/14/23.
+//
+
+import SwiftUI
+
+struct CapsuleText: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    
+    @ViewBuilder let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var userRedText = false
+
+    let motto1 = Text("Draco dormiens")
+    let motto2 = Text("nunquam titillandus")
+
+    @ViewBuilder var spells: some View {
+        Text("Lumos")
+        Text("Obliviate")
+    }
+    
+    var body: some View {
+        VStack {
+            Button("Hello, world!") {
+                print(type(of: self.body))
+            }
+            .frame(width: 200, height: 200)
+            .background(.red)
+
+            Button("Hello World") {
+                userRedText.toggle()
+            }
+            .foregroundColor(userRedText ? .red : .blue)
+        }
+        .padding()
+        
+        VStack {
+            Text("Hello, world!")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.yellow)
+                .padding()
+                .background(.orange)
+            
+            VStack {
+                Text("Gryffindor")
+                    .font(.largeTitle)
+                    .blur(radius: 0)
+                Text("Hufflepuff")
+                Text("Ravenclaw")
+                Text("Slytherin")
+            }
+            .font(.title)
+            .blur(radius: 2)
+        }
+        .padding()
+        
+        VStack {
+            VStack {
+                motto1.foregroundColor(.red)
+                motto2.foregroundColor(.blue)
+            }
+
+            VStack(spacing: 10) {
+                CapsuleText(text: "First")
+                    .foregroundColor(.white)
+                CapsuleText(text: "Second")
+                    .foregroundColor(.yellow)
+            }
+            
+            Text("Hello World")
+                .titleStyle()
+            
+            Color.blue
+                .frame(width: 300, height: 200)
+                .watermarked(with: "Hacking with Swift")
+        }
+        .padding()
+        
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
