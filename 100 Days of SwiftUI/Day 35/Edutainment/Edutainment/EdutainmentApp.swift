706179c7952cb50cb8e11cfbcfa42a78d05ce132
Gabø.montero <gabo.montero89@gmail.com>
2023-10-15 17:45:32 -0400

    Day 35: Consolidation III (Unfinished)
    - Milestone: Projects 4-6

---
+++ 100 Days of SwiftUI/Day 35/Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by ☕️ Gabo.montero on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
