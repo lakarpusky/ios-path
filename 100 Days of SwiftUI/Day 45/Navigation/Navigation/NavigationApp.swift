522694a318ac1481a411c86f5f9010991d8938ea
Gabø.montero <gabo.montero89@gmail.com>
2023-11-12 02:10:08 -0500

    Day 43: Project 9, part 1 (iOS17)
    - The problem with a simple NavigationLink
    - Handling navigation the smart way with navigationDestination()

---
+++ 100 Days of SwiftUI/Day 45/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by ☕️ Gabo.montero on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
