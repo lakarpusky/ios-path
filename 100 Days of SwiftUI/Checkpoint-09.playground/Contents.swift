9d5c9118e451956bc42d48ad9ce9652a4e14a8b3
Gabø.montero <gabo.montero89@gmail.com>
2024-02-03 15:34:17 -0500

    Cleaning up workspace for iOS17 fresh start

--- 100 Days of SwiftUI/Checkpoint-09.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-// .. a func that accepts an optional array of integers, and returns one randomly
-// .. array missing/empty return a random number in the range 1-100
-
-func randomNumber(numbers: [Int]?) -> Int {
-    numbers?.randomElement() ?? Int.random(in: 1...100)
-}
