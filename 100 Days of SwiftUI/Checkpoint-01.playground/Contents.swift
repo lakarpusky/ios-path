fb551704b072131119cf22419b101405052a42ca
lakarpusky <gabo.montero89@gmail.com>
2024-02-07 22:19:07 -0500

    Day 2
    Booleans, string interpolation, and checkpoint 1

--- 100 Days of SwiftUI/Checkpoint-01.playground/Contents.swift
+++ 100 Days of SwiftUI/Checkpoint-01.playground/Contents.swift
@@ -1,3 +1,13 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+// TOPIC: Checkpoint 01
+
+// - Creates a constant honding any temperatue in Celsius
+// - Converts that temperature to Fahrenheit by multiplying by 9, dividing by 5, then adding 32
+// - Prints the result, showing both the Celsius and Fahrenheit values
+
+let celsius = 36.0
+let fahrenheit = (celsius * 9 / 5) + 32
+
+print("Celcius temperature is \(celsius)°")
+print("Fahrenheit temperature is \(fahrenheit)°")
--- 100 Days of SwiftUI/String-interpolation.playground/Contents.swift
+++ 100 Days of SwiftUI/String-interpolation.playground/Contents.swift
@@ -1,3 +1,30 @@
 import Cocoa
 
+// TOPIC: How to join strings together
+
 var greeting = "Hello, playground"
+
+let firstPart = "Hello, "
+let seconPart = "world!"
+let greeting2 = firstPart + seconPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote = "Then he tapped a sign saying \"Belavieve\" and walked away."
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let number = 11
+let missionMessage = "Apollo \(number) landed on the moon."
+print(missionMessage)
+
+// .. can include calculations inside
+print("5 x 5 is \(5 * 5)")
