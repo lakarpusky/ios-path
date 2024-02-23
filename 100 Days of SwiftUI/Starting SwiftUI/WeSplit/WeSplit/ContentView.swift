f79449c04b95a282543c5af8f34b3aeacd97ff8c
lakarpusky <gabo.montero89@gmail.com>
2024-02-22 21:14:19 -0500

    Day 17 - Project 1, part two and three
    - Reading text from the user with TextField
    - Creating pickers in a form
    - Adding a segmented control for tip percentages
    - Calculating the total per person
    - Hiding the keyboard

--- 100 Days of SwiftUI/Starting SwiftUI/WeSplit/WeSplit/ContentView.swift
+++ 100 Days of SwiftUI/Starting SwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -11,38 +11,100 @@ import SwiftUI
 // ...
 // .. for simple properties that are stored in one view
 // .. allow that value to ve stored separately in a place that can be modified
+// .. automatically watches for changes, it will automatically re-invoke the body property
 // .. apple recommends we add "private" access control to those properties
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    @State private var name = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
     
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
+    // .. behave like @State, but designed to handle input focus
+    @FocusState private var amountIsFocused: Bool
+    
+    // .. calculating the total per person
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2) // .. how many people ther are
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let granTotal = checkAmount + tipValue
+        
+        // .. amount per person
+        return granTotal / peopleCount
+    }
+    
+    // .. total amount for the check
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        
+        return checkAmount + tipValue
+    }
     
     var body: some View {
         NavigationStack {
             Form {
                 Section {
-                    Text("hello world")
-                    Button("Tap Count: \(tapCount)") {
-                        tapCount += 1
+                    TextField(
+                        "Amount",
+                        value: $checkAmount,
+                        // .. ask it to treat the input as a currency
+                        // .. ask iOS if it can give us the currency code for the current user
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
+                    )
+                    // .. to show a numeric keyword
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    // .. how many people will share the check
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
                     }
+                    // .. moves the user to a new screen to select their option
+                    .pickerStyle(.navigationLink)
+                }
+                
+                // .. set the section title
+                Section("How much tip do you want to leave?") {
+                    // .. select how much tip they want to leave
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("Amount per person") {
+                    Text(
+                        totalPerPerson,
+                        // .. to format the number as a currency
+                        format: .currency(
+                            code: Locale.current.currency?.identifier ?? "USD"
+                        )
+                    )
                 }
                 
                 Section {
-                    Text("Your name is \(name)")
-                    TextField("Enter your name", text: $name) // .. two-way binding
+                    Text("Total amount for the check: \(totalAmount)")
                 }
-                
-                Picker("Select your student", selection: $selectedStudent) { // .. two-way binding
-                    ForEach(students, id: \.self) { // .. to be able to identify each item uniquely
-                        Text($0)
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
+            // .. add toolbar button when text field is active
+            .toolbar { // .. specify toolbar items for a view
+                if amountIsFocused {
+                    Button("Done") {
+                        // .. to false so that the keyboard is dismissed
+                        amountIsFocused = false
                     }
                 }
             }
-            .navigationTitle("SwiftUI")
-            navigationBarTitleDisplayMode(.inline)
         }
     }
 }
