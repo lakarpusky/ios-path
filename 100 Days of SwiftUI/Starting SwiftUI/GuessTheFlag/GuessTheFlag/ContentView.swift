02e8db16d7b3da6ca4221443d10045d28255fbb9
lakarpusky <gabo.montero89@gmail.com>
2024-02-25 12:00:34 -0500

    Day 22 - Project 2, part three
    - Challenge

--- 100 Days of SwiftUI/Starting SwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 100 Days of SwiftUI/Starting SwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -24,16 +24,39 @@ struct ContentView: View {
     ].shuffled()
     
     @State private var showingScore = false
-    @State private var scoreTitle = ""
+    @State private var showingFinalScore = false
+    @State private var scoreTitle = ""
+    @State private var userScore = 0
+    @State private var questionCount = 8
     
     func flagTapped(_ number: Int) {
-        scoreTitle = number == correctAnswer ? "Corrent" : "Wrong"
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            userScore += 1
+        } else {
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            if userScore > 0 {
+                userScore -= 1
+            }
+        }
         showingScore = true
     }
     
     func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        if questionCount > 1 {
+            questionCount -= 1
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        } else {
+            questionCount -= 1
+            showingFinalScore = true
+        }
+    }
+    
+    func resetGame() {
+        questionCount = 8;
+        userScore = 0
+        showingFinalScore = false
     }
     
     var body: some View {
@@ -41,7 +64,7 @@ struct ContentView: View {
             RadialGradient(
                 stops: [
                     .init(
-                        color: Color(red: 0.1, green: 0.2, blue: 0.45),
+                        color: Color(red: 0.1, green: 0.2, blue: 0.45),
                         location: 0.3
                     ),
                     .init(
@@ -91,9 +114,13 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(userScore)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
+                
+                Text("\(questionCount) questions remaining")
+                    .foregroundStyle(.white)
+                    .font(.subheadline.bold())
                 
                 Spacer()
             }
@@ -102,7 +129,12 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Correct answers: \(userScore)")
+        }
+        .alert("Game ended", isPresented: $showingFinalScore) {
+            Button("Start Over", action: resetGame)
+        } message: {
+            Text("Your final score is \(userScore) 🎉")
         }
     }
 }
