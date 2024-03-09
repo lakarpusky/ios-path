8c9a24917333663097223d31266b3b17f3dba826
lakarpusky <gabo.montero89@gmail.com>
2024-03-09 17:21:13 -0500

    Day 40 - Project 8, part two
    - Loading a specific kind of codable data
    - Using generics to load any kind of Codable data
    - Formatting our mission view

---
+++ 100 Days of SwiftUI/Scaling up to bigger apps/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by ☕️ Gabo.montero on 3/9/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
