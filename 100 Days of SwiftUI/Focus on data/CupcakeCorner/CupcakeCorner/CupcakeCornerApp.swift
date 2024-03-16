5d376485fb0d49bcc0a2153a1251722723e1ad54
lakarpusky <gabo.montero89@gmail.com>
2024-03-16 00:14:17 -0400

    xuserdata update

---
+++ 100 Days of SwiftUI/Focus on data/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by ☕️ Gabo.montero on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100 Days of SwiftUI/Focus on data/CupcakeCorner/CupcakeCorner/ValidatingDisablingForms.swift
@@ -0,0 +1,18 @@
+//
+//  ValidatingDisablingForms.swift
+//  CupcakeCorner
+//
+//  Created by ☕️ Gabo.montero on 3/16/24.
+//
+
+import SwiftUI
+
+struct ValidatingDisablingForms: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ValidatingDisablingForms()
+}
