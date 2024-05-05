075556b0813d9130a8ada7597f82f918382828be
lakarpusky <gabo.montero89@gmail.com>
2024-05-05 04:29:52 -0400

    add git ignore file

---
+++ The Portfolio/FoodOrdering/FoodOrdering/View/ItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemView.swift
+//  FoodOrdering
+//
+//  Created by ☕️ Gabo.montero on 5/5/24.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemView()
+}
