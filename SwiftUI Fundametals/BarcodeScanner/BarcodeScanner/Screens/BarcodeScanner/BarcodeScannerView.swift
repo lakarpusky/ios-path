c7ea1b882c7a1505e931ab9e70732624eee35c03
lakarpusky <gabo.montero89@gmail.com>
2024-05-15 23:29:13 -0400

    Barcode Scanner App Optimizations

--- SwiftUI Fundametals/BarcodeScanner/BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
+++ SwiftUI Fundametals/BarcodeScanner/BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -17,7 +17,7 @@ struct BarcodeScannerView: View {
                     scannedCode: $viewModel.scannedCode,
                     alertItem: $viewModel.alertItem
                 )
-                .frame(maxWidth: .infinity, maxHeight: 300)
+                .frame(maxHeight: 300)
                 
                 Spacer().frame(height: 60)
                 
@@ -33,8 +33,8 @@ struct BarcodeScannerView: View {
             .navigationTitle("Barcode Scanner")
             .alert(item: $viewModel.alertItem) { alertItem in
                 Alert(
-                    title: Text(alertItem.title),
-                    message: Text(alertItem.message),
+                    title: alertItem.title,
+                    message: alertItem.message,
                     dismissButton: alertItem.dismissButton
                 )
             }
--- SwiftUI Fundametals/BarcodeScanner/BarcodeScanner/Utilities/Alert.swift
+++ SwiftUI Fundametals/BarcodeScanner/BarcodeScanner/Utilities/Alert.swift
@@ -9,21 +9,21 @@ import SwiftUI
 
 struct AlertItem: Identifiable {
     let id = UUID()
-    let title: String
-    let message: String
+    let title: Text
+    let message: Text
     let dismissButton: Alert.Button
 }
 
 struct AlertContext {
     static let invalidDeviceInput = AlertItem(
-        title: "Invalid Deive Input",
-        message: "Something is wrong with the camera. We are unable to capture the input.",
+        title: Text("Invalid Deive Input"),
+        message: Text("Something is wrong with the camera. We are unable to capture the input."),
         dismissButton: .default(Text("OK"))
     )
     
     static let invalidScannedType = AlertItem(
-        title: "Invalid Scanned Type",
-        message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.",
+        title: Text("Invalid Scanned Type"),
+        message: Text("The value scanned is not valid. This app scans EAN-8 and EAN-13."),
         dismissButton: .default(Text("OK"))
     )
 }
